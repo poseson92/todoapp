af226bd66f1c2af26ff95308945bdff5e9792eb2
bong <poseson92@gmail.com>
2022-02-12 18:43:24 +0900

    fix: mysql 연결 설정 수정

--- config/config.js
+++ config/config.js
@@ -2,24 +2,24 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "root",
+    username: "admin",
     password: process.env.MYSQL_PASSWORD,
     database: "todolist",
-    host: "127.0.0.1",
+    host: process.env.MYSQL_HOST,
     dialect: "mysql",
   },
   test: {
-    username: "root",
+    username: "admin",
     password: process.env.MYSQL_PASSWORD,
     database: "todolist_test",
-    host: "127.0.0.1",
+    host: process.env.MYSQL_HOST,
     dialect: "mysql",
   },
   production: {
-    username: "root",
+    username: "admin",
     password: process.env.MYSQL_PASSWORD,
     database: "todolist_production",
-    host: "127.0.0.1",
+    host: process.env.MYSQL_HOST,
     dialect: "mysql",
     logging: false,
   },
